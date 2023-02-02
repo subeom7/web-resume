f49533751102081c1f657fbc634394bc0281106a
Subeom7 <subeomkwon@gmail.com>
2023-02-02 18:01:23 -0500

    created nav bar

--- web-resume.js
+++ web-resume.js
@@ -54,3 +54,5 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+// ---------------------------------------------------------------------
